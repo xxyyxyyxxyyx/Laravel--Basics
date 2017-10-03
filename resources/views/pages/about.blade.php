c5dfd3cd81f75ff320a41d9ef0192b8c14958fab
unknown <xxyyxyyxxyyx@gmail.com>
2017-10-03 12:12:48 +0800

    commit

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,14 +1,9 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
+@section('content')
+    <h1>{{$title}}</h1>
+    <p> This is the about page of the application</p>
+@endsection
 
-    <title>{{config('app.name', 'LSAPP')}}</title>
-</head>
-<body>
-<h1>About</h1>
-<p> This is the about page of the application</p>
-</body>
-</html>
+
+
+
